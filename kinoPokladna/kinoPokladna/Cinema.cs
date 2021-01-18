b54b1ae60ea42e2d47580fee57c08daf62505d62
Filip Margitai <u18.415@pslib.cloud>
2021-01-18 13:15:06 +0100

    všechny metody přidány

---
+++ kinoPokladna/kinoPokladna/Cinema.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kinoPokladna
+{
+    class Cinema
+    {
+        private int pocetRadSpod, pocetSedSpod, pocetRadBalk, pocetSedBalk;
+
+        public Cinema(int _pocetRadSpod, int _pocetSedSpod, int _pocetRadBalk, int _pocetSedBalk)
+        {
+            pocetRadSpod = _pocetRadSpod;
+            pocetSedSpod = _pocetSedSpod;
+            pocetRadBalk = _pocetRadBalk;
+            pocetSedBalk = _pocetSedBalk;
+        }
+        public void Main()
+        {
+
+        }
+        private void Movement()
+        {
+
+        }
+        private void ChangeSeatState()
+        {
+
+        }
+        private void Reset()
+        {
+
+        }
+        private void ListSeats()
+        {
+
+        }
+    }
+}
