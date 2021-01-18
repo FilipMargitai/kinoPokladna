38929d3783249ed73d64cdcc1a99ecb3a98c5225
FilipMargitai <filip.margitai@pslib.cz>
2021-01-18 21:07:41 +0100

    Update Cinema.cs

--- kinoPokladna/kinoPokladna/Cinema.cs
+++ kinoPokladna/kinoPokladna/Cinema.cs
@@ -6,18 +6,27 @@ namespace kinoPokladna
 {
     class Cinema
     {
-        private int pocetRadSpod, pocetSedSpod, pocetRadBalk, pocetSedBalk;
-
+        private bool[,] spodPatro { get; set; }
+        private bool[,] balkon { get; set; }
         public Cinema(int _pocetRadSpod, int _pocetSedSpod, int _pocetRadBalk, int _pocetSedBalk)
         {
-            pocetRadSpod = _pocetRadSpod;
-            pocetSedSpod = _pocetSedSpod;
-            pocetRadBalk = _pocetRadBalk;
-            pocetSedBalk = _pocetSedBalk;
+            spodPatro = new bool[_pocetRadSpod, _pocetSedSpod];
+            balkon = new bool[_pocetRadBalk, _pocetSedBalk];
         }
         public void Main()
         {
-
+            PrintArray(spodPatro);
+        }
+        private void PrintArray(bool[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
         private void Movement()
         {
