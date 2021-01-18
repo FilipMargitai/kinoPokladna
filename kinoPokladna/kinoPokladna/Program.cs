18fefa819d78bea10355f40cdf47c640aa4bb5bd
Filip Margitai <u18.415@pslib.cloud>
2021-01-18 10:28:18 +0100

    c# projekt přidán

---
+++ kinoPokladna/kinoPokladna/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kinoPokladna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
